417110964feb59979e991e10028573393bbfc3f3
ashhhlynn <ashleylynnwelch@gmail.com>
2023-03-10 15:58:11 -0600

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
     has_many :order_items
     has_many :orders, through: :order_items
     belongs_to :user
-end
+    validates :image_url, format: {with: /\.(png|jpg)\Z/i}
+    validates :description, length: { maximum: 300 }
+    validates :title, length: { maximum: 100 }
+    validates :price, length: { maximum: 10 }
+
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
     has_many :products
     has_secure_password
     validates :email, uniqueness: { case_sensitive: false }
+    validates :name, length: { maximum: 100 }
+    validates :address, length: { maximum: 100 }
+    validates :city, length: { maximum: 100 }
+    validates :state, length: { maximum: 100 }
+    validates :zip, length: { maximum: 6 }
 end
