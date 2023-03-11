a0f582291cf5bc84d8a9e7098fad54c02137d62a
ashhhlynn <ashleylynnwelch@gmail.com>
2023-03-10 19:40:11 -0600

    added user products order

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,7 @@ class Product < ApplicationRecord
     has_many :orders, through: :order_items
     belongs_to :user
     validates :image_url, format: {with: /\.(png|jpg)\Z/i}
-    validates :description, length: { maximum: 300 }
-    validates :title, length: { maximum: 100 }
-    validates :price, length: { maximum: 10 }
-
+    validates :description, length: { maximum: 170 }
+    validates :title, length: { maximum: 50 }
+    validates :price, length: { maximum: 12 }
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     has_many :orders, -> { where complete: true }
-    has_many :products
+    has_many :products, -> { order 'sold ASC'}
     has_secure_password
     validates :email, uniqueness: { case_sensitive: false }
-    validates :name, length: { maximum: 100 }
+    validates :name, length: { maximum: 50 }
     validates :address, length: { maximum: 100 }
-    validates :city, length: { maximum: 100 }
-    validates :state, length: { maximum: 100 }
-    validates :zip, length: { maximum: 6 }
+    validates :city, length: { maximum: 50 }
+    validates :state, length: { maximum: 50 }
+    validates :zip, length: { maximum: 20 }
 end
